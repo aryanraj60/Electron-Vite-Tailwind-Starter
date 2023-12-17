f039933c195c51870d20651dd69537127347fcd7
aryanraj202 <134063935+aryanraj202@users.noreply.github.com>
2023-12-18 01:10:55 +0530

    first commit

---
+++ src/renderer/src/App.jsx
@@ -0,0 +1,12 @@
+import Versions from './components/Versions'
+import icons from './assets/icons.svg'
+
+function App() {
+  return (
+    <div className="h-screen w-screen bg-black flex items-center justify-center">
+      <h1 className="text-red-500">Enjoy Tailwind Dapu ke Papa 🚀</h1>
+    </div>
+  )
+}
+
+export default App
